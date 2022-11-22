a24148b7125671ac50c3da89ee7758d782fb6788
podyn <podyn@ANDREY>
2022-11-22 07:41:13 +0300

    Initial commit

---
+++ Server/commands.h
@@ -0,0 +1,31 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+#define PASSWORD "123456789"
+
+#define GOOD "true"
+#define BAD "false"
+#define SYSTEM_MESSAGE_START '$'
+
+#define VALIDATION "$connect"
+#define SIGNUP "$signup"
+#define DUPLICATE "$duplicate"
+#define LOG "$log_request"
+#define UPDATE_LOG "$update_log"
+#define LOG_FINISH "$log_finished"
+#define CONNECTED "$connected"
+#define DISCONNECTED "$disconnected"
+#define IS_ONLINE "$is_online"
+#define ADD_CONTACT "$add_contact"
+#define ERROR "$error"
+#define ALL "$all"
+#define ID "$id"
+#define USER "$user"
+#define EXIT "$exit"
+
+
+#define SEND "send"
+#define PHOTO "photo"
+#define FILE "file"
+
+#endif
